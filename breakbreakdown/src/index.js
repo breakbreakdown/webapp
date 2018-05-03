7229100f0e31af8e929b5b3cb2e281ad4065f221
Juan Alvarez <juanalvarezdev@gmail.com>
2018-05-03 11:27:24 -0700

    router

--- breakbreakdown/src/index.js
+++ breakbreakdown/src/index.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+<Router history={customHistory}>
+    <div>
+        <Route path="/login" component={Login}/>
+        <Route path="/app/home" component={Home}/>
+        <Redirect from="/" to="/login"/>
+    </div>
+</Router>
