86dcb34c8fdc8c9db7a27d948bfe54c334e2b025
aqt21 <andrewtran97@gmail.com>
2018-05-17 01:14:59 -0700

    basic graph working

--- breakbreakdown/src/Graph.js
+++ breakbreakdown/src/Graph.js
@@ -2,13 +2,25 @@ import React from 'react';
 import Firebase from 'firebase';
 import M from 'react-materialize';
 import './graph.css';
+import { VictoryPie } from 'victory-pie';
 
 class Graph extends React.Component {
-
+	
+	
 	
 	render() {
 		return (
-		  <div id='graph'> graph </div>
+		  <div id='graph'> 
+			<VictoryPie
+			  colorScale={["tomato", "orange", "gold", "cyan", "navy" ]}
+			  data={[
+				{ x: "Cats", y: 35 },
+				{ x: "Dogs", y: 40 },
+				{ x: "Birds", y: 55 }
+			  ]}
+			  innerRadius={100}
+			/>
+		  </div>
 		);
 	}
 }
