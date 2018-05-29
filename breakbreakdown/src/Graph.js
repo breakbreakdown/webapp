578a5518034be2ecf3aa92fb56a090d55f2f706c
aqt21 <andrewtran97@gmail.com>
2018-05-29 04:48:38 -0700

    graph slices onclick

--- breakbreakdown/src/Graph.js
+++ breakbreakdown/src/Graph.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Firebase from 'firebase';
 import M from 'react-materialize';
+import Materialize from 'materialize-css';
 import './graph.css';
 import { VictoryPie } from 'victory-pie';
 import Countdown from 'react-countdown-now';
+import EventDetails from './EventDetails';
+import $ from 'jquery';
 
 class Graph extends React.Component {
 	constructor(props) {
@@ -15,7 +18,8 @@ class Graph extends React.Component {
 		var d = new Date();
 		var totalMilliseconds = (d.getHours() * 3600000) + (d.getMinutes() * 60000) + (d.getSeconds() * 1000);
 		this.setState({events:[{ x: " ", y: 6, title: "Fortnite Grind" }, { x: " ", y: 1, title: "Info 461 HW" }, { x: " ", y: 2, title: "Capstone Work"}],
-						currTime: totalMilliseconds});
+            currTime: totalMilliseconds
+        });
 	}
 	
 	getTotalEventTime() {
@@ -29,7 +33,6 @@ class Graph extends React.Component {
 	}
 	
 	render() {
-		
 		return (
 		  <div id='graph'> 
 			<VictoryPie
@@ -40,39 +43,34 @@ class Graph extends React.Component {
 			  events={[{
 					  target: "data",
 					  eventHandlers: {
-						onClick: () => {
-						  return [
-							{
-							  target: "data",
-							  mutation: (props) => {
-								const fill = props.style && props.style.fill;
-								return fill === "#c43a31" ? null : { style: { fill: "#c43a31" } };
-							  }
-							}, {
-							  target: "labels",
-							  mutation: (props) => {
-								return props.text === "clicked" ? null : { text: "clicked" };
-							  }
-							}
-						  ];
+                          onClick: () => {
+                              var elems = document.querySelectorAll('.modal');
+                              var instances = Materialize.Modal.init(elems);
+                              var instance = Materialize.Modal.getInstance($('#event-details-popup'));
+                              instance.open();
 						}
 					  }
 					}]}
-			/>
-			<div id="freetime-countdown">
-				<div id="freetime-text"> Free Time </div>
-				<div>
-					<Countdown date={Date.now() + 86400000 - this.state.currTime - this.getTotalEventTime()} />
-				</div>
-			</div>
+                />
+                <div id='event-details-popup' className='modal'>
+                    <div className='modal-content'>
+                        <EventDetails title={this.state.title} />
+                    </div>
+                </div>
+			    <div id="freetime-countdown">
+				    <div id="freetime-text"> Free Time </div>
+				    <div>
+					    <Countdown date={Date.now() + 86400000 - this.state.currTime - this.getTotalEventTime()} />
+				    </div>
+			    </div>
 			
-			<div id="totaltime-left">
-				<div id="totaltime-left-text"> Time Left in Day </div>
-				<div>
-					<Countdown date={Date.now() + 86400000 - this.state.currTime} />
-				</div>
-			</div>
-		  </div>
+			    <div id="totaltime-left">
+				    <div id="totaltime-left-text"> Time Left in Day </div>
+				    <div>
+					    <Countdown date={Date.now() + 86400000 - this.state.currTime} />
+				    </div>
+			    </div>
+		      </div>
 		);
 	}
 }
