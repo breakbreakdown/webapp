3f3e0a980ca035e10cce8439ba68b06069fd47ee
Constance La <you@example.com>
2018-06-05 16:27:28 -0700

    fixed merge errors

--- breakbreakdown/src/AddEventPopup.js
+++ breakbreakdown/src/AddEventPopup.js
@@ -35,35 +35,25 @@ class AddEventPopup extends React.Component {
 		this.setEndTime = this.setEndTime.bind(this);
 		this.createEvent = this.createEvent.bind(this);
         this.handleChange = this.handleChange.bind(this);
-<<<<<<< HEAD
 		this.setDurationHours = this.setDurationHours.bind(this);
 		this.setDurationMinutes = this.setDurationMinutes.bind(this);
-		this.test = this.test.bind(this);
-=======
 		this.getColor = this.getColor.bind(this);
->>>>>>> b0a62198d0043f88aa1e2b62aba554b2e918d9f9
 	}
 
 	componentDidMount() {
 		document.addEventListener('DOMContentLoaded', function () {
-            var date = document.querySelectorAll('.datepicker');
-            var instances = Materialize.Datepicker.init(date);
-            var time = document.querySelectorAll('.timepicker');
-            var instances = Materialize.Timepicker.init(time);
-            var menu = document.querySelectorAll('select');
-            var instances = Materialize.FormSelect.init(menu);
-        });
+			var date = document.querySelectorAll('.datepicker');
+			var instances = Materialize.Datepicker.init(date);
+			var time = document.querySelectorAll('.timepicker');
+			var instances = Materialize.Timepicker.init(time);
+			var menu = document.querySelectorAll('select');
+			var instances = Materialize.FormSelect.init(menu);
+		});
 
 		this.resetForm = this.resetForm.bind(this);
 		this.setEventType();
-<<<<<<< HEAD
-		this.setState({recurrence: '1', hours: '0', minutes: '0'});
+		this.setState({ recurrence: '1', hours: '0', minutes: '0' });
 	}
-=======
-		this.setState({recurrence: '1'});
-	}
-
->>>>>>> b0a62198d0043f88aa1e2b62aba554b2e918d9f9
 	
 	handleChange(evt) {
         this.setState({ [evt.target.id.split('-')[1]]: evt.target.value });
@@ -149,7 +139,7 @@ class AddEventPopup extends React.Component {
 							var durHr = parseFloat(this.state.hours);
 							var durMin = parseFloat(this.state.minutes) / 60;
 							var totalTime = durHr + durMin;
-							
+
 
 							var m = this.state.date[0].getMonth() + 1; //months from 1-12
 							var d = this.state.date[0].getDate();
@@ -174,36 +164,36 @@ class AddEventPopup extends React.Component {
 					}
 
 					eventIdReturn = ApiCalendar.createEvent(this.state.title, this.state.location, this.state.notes, (this.state.colorID + 1) || "1", this.state.start, this.state.end, this.state.recurrence);
-                  alreadyPushed = true;
-                }
-                console.log(eventIdReturn);
-                ApiCalendar.handleSignoutClick();
-            }
-        }.bind(this);
-        ApiCalendar.listenSign(signChanged);
-        // var updates = {};
-        //
-        // //PUT EVERYTHING BELOW HERE IN A FOR LOOP ONCE WE FIGURE OUT HOW TO DO THE LOCAL EVENT OBJECT
-        //
-        // var duration = this.duration;
+					alreadyPushed = true;
+				}
+				console.log(eventIdReturn);
+				ApiCalendar.handleSignoutClick();
 
-        // //An event entry.
-        // //USE THIS ONE FOR SENDING REAL DATA
-        // var eventData = {
-        //   duration: duration
-        // };
-        //
-        // // Get a key for a new event.
-        // //Once we get eventID from google we will use Authorization instead of 'i'
-        // var newPostKey = databaseRef.ref.child('' + 'event ID from google here').key;
-        //
-        // // //adds the event with data into updates array
-        // updates['/days/' + this.date + '/' + newPostKey] = eventData;
-        //
-        // //pushes updates to firebase
-        // return database.ref('users/' + localStorage.getItem('appTokenKey')).update(updates);
-        // console.log('event data pushed');
+			}.bind(this);
+			ApiCalendar.listenSign(signChanged);
+			// var updates = {};
+			//
+			// //PUT EVERYTHING BELOW HERE IN A FOR LOOP ONCE WE FIGURE OUT HOW TO DO THE LOCAL EVENT OBJECT
+			//
+			// var duration = this.duration;
 
+			// //An event entry.
+			// //USE THIS ONE FOR SENDING REAL DATA
+			// var eventData = {
+			//   duration: duration
+			// };
+			//
+			// // Get a key for a new event.
+			// //Once we get eventID from google we will use Authorization instead of 'i'
+			// var newPostKey = databaseRef.ref.child('' + 'event ID from google here').key;
+			//
+			// // //adds the event with data into updates array
+			// updates['/days/' + this.date + '/' + newPostKey] = eventData;
+			//
+			// //pushes updates to firebase
+			// return database.ref('users/' + localStorage.getItem('appTokenKey')).update(updates);
+			// console.log('event data pushed');
+		}
     }
 	
 	setDefaultEndTime(dateStr) {
@@ -307,7 +297,7 @@ class AddEventPopup extends React.Component {
 		}
 	}
 
-<<<<<<< HEAD
+
 	setDurationHours(evt) {
 		this.setState({ hours: evt.target.value });
 	}
@@ -316,19 +306,11 @@ class AddEventPopup extends React.Component {
 		this.setState({ minutes: evt.target.value });
 	}
 
-	test() {
-		console.log($('#event-switch').prop('checked'));
-		console.log(this.state.hours);
-		console.log(this.state.minutes);
-	}
-
-=======
 	getColor(value) {
 		let colors = ['#7986cb', '#33b679', '#8e24aa', '#e67c73', '#f6c026', '#f5511d', '#039be5', '#616161', '#3f51b5', '#0b8043', '#d60000', '#5484ed'];
 		this.setState({colorID: colors.indexOf(value)});
 	}
-	
->>>>>>> b0a62198d0043f88aa1e2b62aba554b2e918d9f9
+
 	render() {
 		var hours = [];
 		for (var i = 1; i < 24; i++) {
@@ -439,7 +421,6 @@ class AddEventPopup extends React.Component {
 						</div>
 						
 						<a className="waves-effect waves-light btn modal-close" id='cancel-btn' onClick={this.resetForm}>Cancel</a>
-						<a className="waves-effect waves-light btn" id='cancel-btn' onClick={this.test}>test</a>
 						
 						<button onClick={(e) => { this.createEvent(e) }} className='btn waves-effect waves-light' type='submit'>Add to Calendar
 							<i className='material-icons right'>send</i>
